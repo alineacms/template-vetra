77ea64ba3258333fa0e3a11f4ab6812350240c00
Ben Merckx <ben@codeurs.be>
2025-07-10 15:27:36 +0200

    Force alinea dev

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
+import {execSync} from 'node:child_process'
 import {withAlinea} from 'alinea/next'
 
+// Bolt seems to default to `npx next dev` so force it to use alinea dev
+const isAlineaDev = process.env.ALINEA_DEV_SERVER
+if (!isAlineaDev) {
+  try {
+    execSync('npx alinea dev -- next dev', {
+      stdio: 'inherit'
+    })
+    process.exit(0)
+  } catch (error) {
+    console.error('Failed to start alinea dev:', error)
+    process.exit(1)
+  }
+}
+
 export default withAlinea({
-  eslint: { 
-    ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true
   }
 })
