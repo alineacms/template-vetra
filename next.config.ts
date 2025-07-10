76f0482976ef60afdaaf63922998b9d3c2fa46eb
Ben <ben@codeurs.be>
2025-07-10 14:49:50 +0200

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
 import {withAlinea} from 'alinea/next'
 
-const isAlineaDev = process.env.ALINEA_DEV_SERVER
-if (!isAlineaDev) {
-  try {
-    execSync('npx alinea dev -- next dev', {
-      stdio: 'inherit'
-    })
-    process.exit(0)
-  } catch (error) {
-    console.error('Failed to start alinea dev:', error)
-    process.exit(1)
-  }
-}
-
 export default withAlinea({
   eslint: { 
     ignoreDuringBuilds: true,
